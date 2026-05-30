78fc34234f1c6afb529d6cf4a244d60585ae3e55
Daniel Bastos <danbastos@gmail.com>
2026-05-30 12:58:28 -0300

    configurando inertia + vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+    message: String,
+});
+</script>
+
+<template>
+    <main>
+        <h1>{{ message }}</h1>
+    </main>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
+Route::get('/', fn () => Inertia::render('Home', [
+    'message' => 'Olá, Inertia + Vue!',
+]));
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
