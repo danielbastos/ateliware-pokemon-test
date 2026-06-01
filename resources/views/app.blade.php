078a264ba86348991715627f893922c712d8ccef
Daniel Bastos <danbastos@gmail.com>
2026-05-31 22:59:43 -0300

    Adicionando favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
